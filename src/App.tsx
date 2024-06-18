1b38f40cf2452c222e3a41eba239c0b160e3fecf
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-06-17 18:05:16 -0700

    add basic experience page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
     <>
       <Navbar />
       <About />
+      <Experience />
     </>
   );
 }
