deb6113c865a8aaca924474e825e8e0eda82aa12
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-06-19 17:58:23 -0700

    make about page full screen, start layout for experience page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,39 +2,42 @@ import headshot from "../assets/headshot.jpg";
 
 function About() {
   return (
-    <div className="about-page grid grid-cols-1 sm:grid-cols-2 items-stretch gap-y-8 sm:gap-y-0">
-      <div className="rounded w-full sm:w-3/4 mx-auto h-auto">
-        <div className="space-y-8 px-8 sm:px-0">
-          <h1 className="text-5xl font-semibold">About</h1>
-          <p className="text-custom-grey-text text-2xl">
-            Hello, I am Brian Tsou!
-          </p>
-          <p className="text-custom-blue-text text-xl">
-            "Excepteur efficient emerging, minim veniam anim aute carefully
-            curated Ginza conversation exquisite perfect nostrud nisi intricate
-            Content. Qui international first-class nulla ut. Punctual
-            adipisicing, essential lovely queen tempor eiusmod irure. Exclusive
-            izakaya charming Scandinavian impeccable aute quality of life soft
-            power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip,
-            et Porter destination Toto remarkable officia Helsinki excepteur
-            Basset hound. Zürich sleepy perfect consectetur."
-          </p>
-          <div className="text-center sm:text-left">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-              Resume
-            </button>
+    <div className="h-screen">
+      <div className="about-page grid grid-cols-1 sm:grid-cols-2 items-stretch gap-y-8 sm:gap-y-0">
+        <div className="rounded w-full sm:w-3/4 mx-auto h-auto">
+          <div className="space-y-8 px-8 sm:px-0">
+            <h1 className="text-5xl font-semibold">About</h1>
+            <p className="text-custom-grey-text text-2xl">
+              Hello, I am Brian Tsou!
+            </p>
+            <p className="text-custom-blue-text text-xl">
+              "Excepteur efficient emerging, minim veniam anim aute carefully
+              curated Ginza conversation exquisite perfect nostrud nisi
+              intricate Content. Qui international first-class nulla ut.
+              Punctual adipisicing, essential lovely queen tempor eiusmod irure.
+              Exclusive izakaya charming Scandinavian impeccable aute quality of
+              life soft power pariatur Melbourne occaecat discerning. Qui
+              wardrobe aliquip, et Porter destination Toto remarkable officia
+              Helsinki excepteur Basset hound. Zürich sleepy perfect
+              consectetur."
+            </p>
+            <div className="text-center sm:text-left">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Resume
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="sm:relative w-full overflow-hidden">
-        <img
-          src={headshot}
-          alt="Headshot photo"
-          className="rounded-md w-3/4 mx-auto h-full object-scale-down"
-        ></img>
+        <div className="sm:relative w-full overflow-hidden">
+          <img
+            src={headshot}
+            alt="Headshot photo"
+            className="rounded-md w-3/4 mx-auto h-full object-scale-down"
+          ></img>
+        </div>
       </div>
     </div>
   );
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -30,16 +30,42 @@ function FlexItem({ children, spacingClass, visualClass }: FlexItemProps) {
 
 function Experience() {
   return (
-    <div className="experience-page flex flex-row gap-y-8 sm:gap-y-0">
-      <div className="rounded w-full sm:w-3/4 mx-auto h-auto">
-        <FlexItem spacingClass="py-8" visualClass="bg-blue-100">
-          <div className="basis-3/4 bg-green-100">3/4</div>
-          <div className="basis-1/4 bg-green-400">1/4</div>
-          <div className="space-y-8 sm:px-0">
+    <div>
+      <div className="flex justify-center items-center">
+        {" "}
+        <div className="grid grid-cols-1 place-items-center">
+          {" "}
+          {/* Use grid-cols-1 if you want a single column layout */}
+          <div className="space-y-8">
             <h1 className="text-5xl font-semibold">Experience</h1>
             <p className="text-custom-grey-text text-2xl">View LinkedIn</p>
           </div>
-        </FlexItem>
+        </div>
+      </div>
+      {/* 2nd row */}
+      <div className="grid grid-cols-3 place-items-center">
+        <GridItem spacingClass="col-span-2" visualClass="bg-blue-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6 mb-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z"
+            />
+          </svg>
+          <div className="space-y-2 px-8 sm:px-0">
+            <h1 className="text-2xl">Systems/Software QA Analyst</h1>
+            <p className="text-custom-grey-text text-lg">
+              Gatekeeper Systems - Abbotsford, BC
+            </p>
+          </div>
+        </GridItem>
       </div>
     </div>
   );
