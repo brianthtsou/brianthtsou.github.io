256e5f769dead11327f72b55a8b495a39884ee49
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-06-26 18:48:58 -0700

    styling change to projectitem

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -11,10 +11,12 @@ function ProjectItem({ svg, title, date, content }: ProjectItemProps) {
       <div className="my-6 px-8 sm:px-0 flex justify-center items-center">
         {svg}
       </div>
-      <div className="space-y-2 px-8 sm:px-0">
+      <div className="space-y-2 my-6 px-8 sm:px-0 flex-col flex justify-center items-center">
         <h1 className="text-2xl">{title}</h1>
         <p className="text-custom-grey-text text-lg">{date}</p>
-        <p className="text-custom-blue-text text-lg">{content}</p>
+        <p className="text-custom-blue-text text-lg text-center mx-6">
+          {content}
+        </p>
       </div>
     </div>
   );
@@ -32,6 +34,31 @@ function Projects() {
       </div>
       {/* Project grid */}
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-y-6 gap-x-6">
+        <ProjectItem
+          svg={
+            <svg
+              className="h-20 w-20 text-slate-500"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path stroke="none" d="M0 0h24v24H0z" />{" "}
+              <path d="M14 3v4a1 1 0 0 0 1 1h4" />{" "}
+              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />{" "}
+              <circle cx="11" cy="16" r="1" />{" "}
+              <polyline points="12 16 12 11 14 12" />
+            </svg>
+          }
+          title="Spotify Playlist Generator"
+          date="Jan 2020 - Jan 2021"
+          content="Another subheading—maybe it’s related to the image on the left, or the button below "
+        ></ProjectItem>
         <ProjectItem
           svg={
             <svg
