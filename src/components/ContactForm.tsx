dab7d40b4c1f22e7a61b2458f703f1a759b1e5f8
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-08-27 14:04:29 -0700

    separate contact form into its own component

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,70 @@
+const ContactForm = () => {
+  return (
+    <div className="space-y-4">
+      <div className="sm:flex sm:space-x-6">
+        <div className="flex-1">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            First name
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              className="block w-full rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+            ></input>
+          </div>
+        </div>
+        <div className="flex-1">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Last name
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              className="block w-full rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="block w-full rounded-md border-0 pl-2 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          ></input>
+        </div>
+      </div>
+      <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          Your Message
+        </label>
+        <div className="mt-2">
+          <input
+            id="message"
+            name="message"
+            type="message"
+            className="block w-full rounded-md border-0 pl-2 pt-2 pb-20 text-top text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          ></input>
+        </div>
+      </div>
+      <div className="text-center sm:text-left">
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
