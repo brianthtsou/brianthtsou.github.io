89cdfc63f6d41e64d684fe48d2c2c4c2866b3e71
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-08-28 14:47:51 -0700

    add email sending functionality

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,12 +8,14 @@ const ContactForm = () => {
     e.persist();
     e.preventDefault();
     setIsSubmitting(true);
+    console.log(import.meta.env.VITE_SERVICE_ID);
+
     emailjs
       .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
         e.target,
-        process.env.REACT_APP_PUBLIC_KEY
+        import.meta.env.VITE_PUBLIC_KEY
       )
       .then(
         (result) => {
@@ -95,7 +97,7 @@ const ContactForm = () => {
         </div>
         <div className="text-center sm:text-left">
           <button
-            type="button"
+            type="submit"
             disabled={isSubmitting}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
